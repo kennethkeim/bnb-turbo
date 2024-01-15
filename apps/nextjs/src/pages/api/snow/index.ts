d18816843f09ae40eaff464fca32936a0dd27541
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2024-01-15 09:42:48 -0500

    wip: get max depth

--- apps/nextjs/src/pages/api/snow/index.ts
+++ apps/nextjs/src/pages/api/snow/index.ts
@@ -1,12 +1,13 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 import { handleApiError } from "~/utils/exceptions";
+import { logger } from "~/utils/logger";
 import { MathUtil } from "~/utils/math";
 import { allowMethods, auth } from "~/utils/request";
 import { getSnowDepth } from "~/utils/weather";
 import { type LatLng } from "~/models/locations";
 
-const FORECAST_DAYS = 7;
+const FORECAST_DAYS = 5;
 const LANC: LatLng = { latitude: 40.0379, longitude: -76.3055 };
 
 export default async function handler(
@@ -22,7 +23,12 @@ export default async function handler(
     const rounded = snowDepth.hourly.map((hr) => {
       return { ...hr, snowDepth: MathUtil.roundTo2Decimals(hr.snowDepth) };
     });
-    console.table(rounded);
+
+    const maxDepth = rounded.reduce((acc, current) => {
+      const larger = current.snowDepth > acc.snowDepth ? current : acc;
+      return { ...larger };
+    });
+    logger.info(`Max snow depth is ${maxDepth.snowDepth} at ${maxDepth.time}`);
 
     res.status(200).json({
       message: `I have alerted the humans to rid the Airbnb of snow and ice.`,
